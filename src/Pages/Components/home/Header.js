365b918e61ab8a270b8e56a1f3a0ebf2c7d20f56
optigo-app <130429763+optigo-app@users.noreply.github.com>
2023-10-26 17:43:17 +0530

    create cart page , JEWELLERY PAGE

--- src/Pages/Components/home/Header.js
+++ src/Pages/Components/home/Header.js
@@ -9,11 +9,12 @@ export default function Header() {
     <div style={{ height: '150px', border: '1px solid black' }}>
       header
       <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <h5 style={{ margin: '20px' , cursor : 'pointer' }} onClick={() => navigation('/CartDetail')}>cart</h5>
+
         <h5 style={{ margin: '20px' , cursor : 'pointer' }} onClick={() => navigation('/RegisterPage')}>REGISTER</h5>
 
         <h5 style={{ margin: '20px' , cursor : 'pointer' }} onClick={() => navigation('/LoginPage')}>LOGIN</h5>
       </div>
-
     </div>
   )
 }
