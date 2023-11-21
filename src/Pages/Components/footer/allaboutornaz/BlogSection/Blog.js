cd3917ab885221eff28548b45b8b8aef39a4bdd0
optigo-app <130429763+optigo-app@users.noreply.github.com>
2023-11-21 10:08:29 +0530

    blog slider banner setting

--- src/Pages/Components/footer/allaboutornaz/BlogSection/Blog.js
+++ src/Pages/Components/footer/allaboutornaz/BlogSection/Blog.js
@@ -1,7 +1,57 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import bg1 from '../../../../assets/blog-banner/blog-slider-big-1.jpg'
+import bg2 from '../../../../assets/blog-banner/blog-slider-big-2.jpeg'
+import bg3 from '../../../../assets/blog-banner/blog-slider-big-3.jpg'
 
 export default function Blog() {
   return (
-    <div>Blog</div>
+    <div style={{display:'flex',justifyContent:'center'}}>
+      <div style={{marginTop:'60px',height:'600px',width:'1140px'}}>
+      <Carousel fade indicators={false}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={bg1}
+          alt="First slide"
+          style={{height:'600px',width:'1140px',objectFit:'cover'}}
+        />
+        <Carousel.Caption style={{top:'240px'}}>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={bg2}
+          alt="Second slide"
+          style={{height:'600px',width:'1140px',objectFit:'cover'}}
+        />
+        <Carousel.Caption style={{top:'240px'}}>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={bg3}
+          alt="Third slide"
+          style={{height:'600px',width:'1140px',objectFit:'cover'}}
+        />
+        <Carousel.Caption style={{top:'240px'}}>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
+    </div>
   )
 }
