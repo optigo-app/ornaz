d5241676e812cc720ae4e2417412aabf12b2b6b9
optigo-app <130429763+optigo-app@users.noreply.github.com>
2023-11-27 18:14:47 +0530

    add header section

--- src/ALL-Theme/SMININGROCKS/Pages/Components/home/topVideo/Video.js
+++ src/ALL-Theme/SMININGROCKS/Pages/Components/home/topVideo/Video.js
@@ -3,7 +3,7 @@ import './Video.css'
 
 export default function Video() {
   return (
-    <div style={{ paddingInline: '6%' }}>
+    <div>
 
       <video width="500" autoPlay controls loop style={{ height: 'auto', width: '100%' }}>
         <source src="https://player.vimeo.com/progressive_redirect/playback/648405863/rendition/1080p/file.mp4?loc=external&amp;log_user=0&amp;signature=caa083b1c8ffc0d906e9525db5c0b51445b4a915857f462c786d974f5278c22f" type="video/mp4" />
