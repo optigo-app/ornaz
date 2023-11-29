43f151699c5e6208d97a1b962ca0c391431fd255
optigo-app <130429763+optigo-app@users.noreply.github.com>
2023-11-29 13:38:47 +0530

    imapct page done (explore more remaining)

--- src/ALL-Theme/SMININGROCKS/Pages/Components/Impact/campaign/Campaign.js
+++ src/ALL-Theme/SMININGROCKS/Pages/Components/Impact/campaign/Campaign.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ImpCards from "../impact-cards/ImpCards";
+import SustainAbility from "../../sustainAbility/SustainAbility";
+import jointlogo from '../../../assets/Impact/campaign/jointlogo.avif'
+import united from '../../../assets/Impact/campaign/united.jpg'
 
 const Campaign = () => {
   return (
@@ -41,6 +44,33 @@ const Campaign = () => {
       </div>
 
       <ImpCards/>
+      <div style={{padding: "0px 85px 0px",marginTop:'55px'}}  >
+      <SustainAbility/>
+      </div>
+      <div style={{padding: "0px 85px 0px",marginTop:'45px'}}>
+          <img src={united} alt={''}/>
+      </div>
+      <div style={{padding: "0px 85px 0px",marginTop:'45px'}}>
+        <div style={{display:'flex',justifyContent:'center'}}>
+          <img src={jointlogo} alt={''}/>
+        </div>
+        <div style={{textAlign:'center',display:'flex',flexDirection:'row',justifyContent:'center',paddingBottom:'30px'}}>
+          <div style={{width:'568px',marginTop:'25px',color:'#7d7f85'}}>
+          <p style={{fontFamily:'FreightDisp Pro Medium',fontSize:'26px'}}>Keeping The Promise</p>
+          <p style={{fontFamily:'TT Commons, sans-serif',fontSize:'13px'}}>
+            Together, we will create an unstoppable force in achieving the <space/>
+             <b>Sustainable Development Goals</b>. With just nine years left the 
+            time has come to deliver on our 2030 promise.
+          </p>
+          <p style={{fontFamily:'TT Commons, sans-serif',fontSize:'13px'}}>
+            Together, we can deliver the extraordinary and we must keep 
+            our promise for the Global Goals and create the world we 
+            want to see today and for our future.
+          </p>
+          <p style={{fontFamily:'TT Commons, sans-serif',fontSize:'13px'}}> <u style={{cursor:'pointer'}}>Donate Now</u> Directly to UNSDG fund!</p>
+          </div>
+        </div>
+      </div>
       
     </div>
   );
