a5032fb0c587cd72012a11a8bb2066c417e5a414
optigo-app <130429763+optigo-app@users.noreply.github.com>
2023-12-13 18:02:48 +0530

    bug solved

--- src/ALL-Theme/GORJANA/Pages/Components/home/Header/Header.js
+++ src/ALL-Theme/GORJANA/Pages/Components/home/Header/Header.js
@@ -1,145 +1,119 @@
-import React from 'react'
+import React from "react";
 import { PiMapPinLight } from "react-icons/pi";
-import './Header.css'
-import { useNavigate } from 'react-router-dom';
+import "./Header.css";
+import { useNavigate } from "react-router-dom";
 import { IoSearch } from "react-icons/io5";
 import { HiOutlineShoppingBag } from "react-icons/hi2";
-import { useSetRecoilState } from 'recoil';
-import { BlurFlag } from '../../../recoil';
-
+import { useSetRecoilState } from "recoil";
+import { BlurFlag } from "../../../recoil";
 
 import { IoMenuOutline } from "react-icons/io5";
 
 export default function Header({}) {
+  let navigate = useNavigate();
 
-    let navigate = useNavigate()
+  let setRecoilBlurFlag = useSetRecoilState(BlurFlag);
 
-    let setRecoilBlurFlag = useSetRecoilState(BlurFlag)
-
-    return (
-      <div>
-        <div className="gorjanaHeader">
-          <div className="gorjanaTopHeader">
-            <PiMapPinLight style={{ height: "25px", width: "40px" }} />
-            <a href="/pages/store-locator" style={{ fontSize: "12px" }}>
-              Find a Store Near You
-            </a>
-          </div>
-          <div className="gorajanaBottomHeaderMain">
-            <div className="gorjanaImgMenuMain">
-              <img
-                src="https://www.gorjana.com/cdn/shop/t/1511/assets/logo-light.svg?v=36596364826355077531701378483&em-format=avif"
-                width="172px"
-                className="gorjanaHederImage"
-                onClick={() => navigate("/")}
-              />
-              <div
-                style={{
-                  display: "flex",
-                  width: "100%",
-                  justifyContent: "space-between",
-                  alignContent: "center",
-                }}
-              >
-                <ul className="gorjanaHeaderMenu">
-                  <li
-                    className="gorjana-Menu-item"
-                    onClick={() => setRecoilBlurFlag(true)}
-                  >
-                    Jewelry
-                  </li>
-                  <li
-                    className="gorjana-Menu-item"
-                    onClick={() => setRecoilBlurFlag(true)}
-                  >
-                    Fine Jewelry
-                  </li>
-                  <li
-                    className="gorjana-Menu-item"
-                    onClick={() => setRecoilBlurFlag(true)}
-                  >
-                    New Arrivals
-                  </li>
-                  <li
-                    className="gorjana-Menu-item"
-                    onClick={() => setRecoilBlurFlag(true)}
-                  >
-                    Best Sellers
-                  </li>
-                  <li
-                    className="gorjana-Menu-item"
-                    onClick={() => setRecoilBlurFlag(true)}
-                  >
-                    Gifts
-                  </li>
-                  <li
-                    className="gorjana-Menu-item"
-                    onClick={() => setRecoilBlurFlag(true)}
-                  >
-                    Explore
-                  </li>
-                </ul>
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    marginRight: "55px",
-                  }}
-                >
-                  <IoSearch
-                    style={{
-                      height: "25px",
-                      cursor: "pointer",
-                      width: "25px",
-                      marginInline: "15px",
-                    }}
-                  />
-                  <p
-                    style={{
-                      marginInline: "15px",
-                      cursor: "pointer",
-                      margin: "10px",
-                    }}
-                  >
-                    Sign In
-                  </p>
-                  <HiOutlineShoppingBag
-                    style={{
-                      height: "25px",
-                      cursor: "pointer",
-                      width: "25px",
-                      marginInline: "15px",
-                    }}
-                  />
-                </div>
-                <div className='gorajanaBottomHeaderMain'>
-                    <div className='gorjanaImgMenuMain'>
-                        <img src='https://www.gorjana.com/cdn/shop/t/1511/assets/logo-light.svg?v=36596364826355077531701378483&em-format=avif' width='172px' className='gorjanaHederImage'
-                            onClick={() => navigate('/')}
-                        />
-                        <div className='gorjanaHeaderSubMenuMain' >
-                            <ul className='gorjanaHeaderMenu'>
-                                <li className='gorjana-Menu-item' onClick={() => navigate('/jewelry')}>Jewelry</li>
-                                <li className='gorjana-Menu-item'>Fine Jewelry</li>
-                                <li className='gorjana-Menu-item'>New Arrivals</li>
-                                <li className='gorjana-Menu-item'>Best Sellers</li>
-                                <li className='gorjana-Menu-item'>Gifts</li>
-                                <li className='gorjana-Menu-item'>Explore</li>
-                            </ul>
-                            <div className='gorjanaHeaderMenuIconeWeb' >
-                                <IoSearch style={{height: '25px',cursor:'pointer',width: '25px' , marginInline: '15px'}}/>
-                                <p style={{marginInline: '15px' ,cursor:'pointer',margin: '10px'}} onClick={() => navigate('/Signin')}>Sign In</p>
-                                <HiOutlineShoppingBag style={{height: '25px',cursor:'pointer',width: '25px' , marginInline: '15px'}} />
-                            </div>
-                            <div className='gorjanaHeaderMenuIconeMobile' >
-                                <IoSearch className='gorHeaderMobileIcoen'/>
-                                <HiOutlineShoppingBag className='gorHeaderMobileIcoen' />
-                                <IoMenuOutline className='gorHeaderMobileIcoen'/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="gorjanaHeader">
+        <div className="gorjanaTopHeader">
+          <PiMapPinLight style={{ height: "25px", width: "40px" }} />
+          <a href="/pages/store-locator" style={{ fontSize: "12px" }}>
+            Find a Store Near You
+          </a>
         </div>
-    )
+        <div className="gorajanaBottomHeaderMain">
+          <div className="gorjanaImgMenuMain">
+            <img
+              src="https://www.gorjana.com/cdn/shop/t/1511/assets/logo-light.svg?v=36596364826355077531701378483&em-format=avif"
+              width="172px"
+              className="gorjanaHederImage"
+              onClick={() => navigate("/")}
+            />
+            <div
+              style={{
+                display: "flex",
+                width: "100%",
+                justifyContent: "space-between",
+                alignContent: "center",
+              }}
+            >
+              <ul className="gorjanaHeaderMenu">
+                <li
+                  className="gorjana-Menu-item"
+                  onClick={() => setRecoilBlurFlag(true)}
+                >
+                  Jewelry
+                </li>
+                <li
+                  className="gorjana-Menu-item"
+                  onClick={() => setRecoilBlurFlag(true)}
+                >
+                  Fine Jewelry
+                </li>
+                <li
+                  className="gorjana-Menu-item"
+                  onClick={() => setRecoilBlurFlag(true)}
+                >
+                  New Arrivals
+                </li>
+                <li
+                  className="gorjana-Menu-item"
+                  onClick={() => setRecoilBlurFlag(true)}
+                >
+                  Best Sellers
+                </li>
+                <li
+                  className="gorjana-Menu-item"
+                  onClick={() => setRecoilBlurFlag(true)}
+                >
+                  Gifts
+                </li>
+                <li
+                  className="gorjana-Menu-item"
+                  onClick={() => setRecoilBlurFlag(true)}
+                >
+                  Explore
+                </li>
+              </ul>
+              <div className="gorjanaHeaderMenuIconeWeb">
+                <IoSearch
+                  style={{
+                    height: "25px",
+                    cursor: "pointer",
+                    width: "25px",
+                    marginInline: "15px",
+                  }}
+                />
+                <p
+                  style={{
+                    marginInline: "15px",
+                    cursor: "pointer",
+                    margin: "10px",
+                  }}
+                  onClick={() => navigate("/Signin")}
+                >
+                  Sign In
+                </p>
+                <HiOutlineShoppingBag
+                  style={{
+                    height: "25px",
+                    cursor: "pointer",
+                    width: "25px",
+                    marginInline: "15px",
+                  }}
+                />
+              </div>
+              <div className="gorjanaHeaderMenuIconeMobile">
+                <IoSearch className="gorHeaderMobileIcoen" />
+                <HiOutlineShoppingBag className="gorHeaderMobileIcoen" />
+                <IoMenuOutline className="gorHeaderMobileIcoen" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
