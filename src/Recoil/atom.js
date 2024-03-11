48c3790de73f112cf7a3db9ebfa46ad29715038a
optigo-app <130429763+optigo-app@users.noreply.github.com>
2024-03-11 11:49:59 +0530

    header filter changes

--- src/Recoil/atom.js
+++ src/Recoil/atom.js
@@ -1,20 +1,21 @@
 import { atom } from "recoil";
 
-
-
 export const CartListCounts = atom({
-    key: 'CartListCounts',
-    default: 0,
-  });
+  key: 'CartListCounts',
+  default: 0,
+});
 
-  export const WishListCounts = atom({
-    key: 'WishListCounts',
-    default: 0,
-  });
+export const WishListCounts = atom({
+  key: 'WishListCounts',
+  default: 0,
+});
 
+export const loginState = atom({
+  key: 'loginState',
+  default: false,
+})
 
-
-  export const loginState = atom({
-    key: 'loginState',
-    default: false,
-  })
+export const HeaderData = atom({
+  key: 'HeaderData',
+  default: []
+})
